d78228f2c53cff0ec0bab93b5616e44bee96ebf1
Kim CHOUARD <github@chouard.kim>
2024-02-01 16:42:02 +0100

    Unexpected Link href external url behavior

--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -15,30 +15,37 @@ export default function EditScreenInfo({ path }: { path: string }) {
           style={styles.getStartedText}
           lightColor="rgba(0,0,0,0.8)"
           darkColor="rgba(255,255,255,0.8)">
-          Open up the code for this screen:
+          The link should point to:
         </Text>
 
         <View
           style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
           darkColor="rgba(255,255,255,0.05)"
           lightColor="rgba(0,0,0,0.05)">
-          <MonoText>{path}</MonoText>
+          <MonoText>http://testurl.com/path/with(parenthesis)/file.ext</MonoText>
         </View>
 
         <Text
           style={styles.getStartedText}
           lightColor="rgba(0,0,0,0.8)"
           darkColor="rgba(255,255,255,0.8)">
-          Change any of the text, save the file, and your app will automatically update.
+          But ends up pointing to (Web only, works on native):
         </Text>
+
+        <View
+          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
+          darkColor="rgba(255,255,255,0.05)"
+          lightColor="rgba(0,0,0,0.05)">
+          <MonoText>http://testurl.com/path/file.ext</MonoText>
+        </View>
       </View>
 
       <View style={styles.helpContainer}>
         <ExternalLink
           style={styles.helpLink}
-          href="https://docs.expo.io/get-started/create-a-new-app/#opening-the-app-on-your-phonetablet">
+          href="http://testurl.com/path/with(parenthesis)/file.ext">
           <Text style={styles.helpLinkText} lightColor={Colors.light.tint}>
-            Tap here if your app doesn't automatically update after making changes
+            Test Link
           </Text>
         </ExternalLink>
       </View>
